d70ac3167fbb5dd8c58c4a9332c5b0ca622d84b1
aws1313 <69417093+aws1313@users.noreply.github.com>
2024-02-23 10:32:41 +0100

    update sensor

--- sensors/gpssensor.py
+++ sensors/gpssensor.py
@@ -1,7 +1,9 @@
-import gps
 import time
+
+from gpsdclient import GPSDClient
+
 from models.sensor import Sensor, SensorType, SensorData
-from threading import Lock
+from threading import Lock, Thread
 import logging
 class GPSSensor(Sensor):
     def __init__(self, _id:int, serial_lock: Lock):
@@ -9,12 +11,26 @@ class GPSSensor(Sensor):
         self.serial_lock = serial_lock
         self.sensor_type = SensorType.GPS
         self.logger = logging.getLogger("gps-sensor_" + str(_id))
-        self.session = gps.gps(mode=gps.WATCH_ENABLE)
+        self.session = GPSThread()
+        self.session.start()
         super().__init__(self.sensor_type)
 
     def get_data(self) -> SensorData:
         with self.serial_lock:
             res = []
         return SensorData(self.id, self.sensor_type, res)
+
+class GPSThread(Thread):
+    def __init__(self):
+        super(GPSThread, self).__init__()
+        self.last_data = {}
+        self.daemon = True
+
+    def run(self) -> None:
+        while True:
+            with GPSDClient() as client:
+                for result in client.dict_stream(convert_datetime=True, filter=["TPV"]):
+                    self.last_data = result
+
 if __name__ == '__main__':
-    session = gps.gps(mode=gps.WATCH_ENABLE)
+    pass
